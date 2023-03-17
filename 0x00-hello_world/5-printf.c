1965d637d3ee8531afcfe7722f35a6ef314d7422
omonuj <jonahodoh@gmail.com>
2023-03-17 08:21:43 -0700

    alx low lang

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point, program that prints the outcome of a  piece of art
+ * Return: 0 (success)
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
