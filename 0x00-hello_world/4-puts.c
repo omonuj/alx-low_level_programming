f0bd057cfe3c78ff0e84c79a7c78781f9f95c1e2
omonuj <jonahodoh@gmail.com>
2023-03-17 08:06:40 -0700

    alx low lang

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- *main - Entry point, program that is fun
- *Return: is 0
+ *main - Entry point, program that prints a line with puts function
+ *Return: 0 (success)
  *
  */
 int main(void)
