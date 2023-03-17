97acf10a618367250a0b2ee6ccc7f68988dc85ea
omonuj <jonahodoh@gmail.com>
2023-03-17 07:55:35 -0700

    alx low lang

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - Entry point, program that is fun
+ *Return: is 0
+ *
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
