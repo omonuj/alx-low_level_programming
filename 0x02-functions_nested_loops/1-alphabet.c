b15388c0b9b63c68ae1a4a1ad52c5424da2b791b
omonuj <jonahodoh@gmail.com>
2023-03-21 15:02:04 -0700

    task 0

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - prints the lowercase alphabets
+ *
+ */
+void print_alphabet(void)
+{
+	int j = 'a';
+	while (j <= 'z')
+{
+	_putchar(j);
+	j++;
+}
+	_putchar('\n');
+}
