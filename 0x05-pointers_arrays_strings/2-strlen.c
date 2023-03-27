e0969bf3af895246f9055f15baabd87840486d28
omonuj <jonahodoh@gmail.com>
2023-03-27 16:43:09 -0700

    hot

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+i#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - returns length of a string
+ * @s: parameter
+ * Return: Always o.
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
