87c35a4fb1e8cd5e4cc14057578c9ef40ff2b92e
omonuj <jonahodoh@gmail.com>
2023-03-27 17:32:33 -0700

    hot

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,8 +11,9 @@ int _atoi(char *s)
 	unsigned int num = 0;
 
 	do {
-		if (*s == '_')
+		if (*s == '-')
 			sign *= -1;
+
 		else if (*s >= '0' && *s <= '9')
 			num = (num * 10) + (*s - '0');
 		else if (num > 0)
