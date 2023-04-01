5963fef9bcbfa6e20009b44bd65cb417b6cbd10e
omonuj <jonahodoh@gmail.com>
2023-03-31 17:12:45 -0700

    array

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,27 +1,24 @@
 #include <stdio.h>
 /**
- * main - entry point
- * Return: 0
+ * main- Entry point
+ * Return: return 0
  */
 int main(void)
 {
 	int n;
-	int a[5];
+	int b[5];
 	int *p;
 
-	a[2] = 1024;
+	b[2] = 1024;
 	p =  &n;
-	/*
-	 * write your line of code here...
-	 * Remeber:
-	 * -you are not allowed to use a
+	/**
+	 * Remeber - you are not allowed to use a
 	 * - you are not allowed to modify p
-	 * - only one statement
-	 * - you are not allowed to code anything else than this line f code
+	 *   - only one statemente
 	 */
 	*(p + 5) = 98;
 	/*...so that this prints 98\n */
-
-	printf("a[2] %&d\n", a[2]);
+	printf("b[2] %d\n", b[2]);
 	return (0);
 }
+
