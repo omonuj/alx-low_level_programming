94981dade88bc9f053c688a914e57f42f7d7c7c9
omonuj <jonahodoh@gmail.com>
2023-04-03 17:55:21 -0700

    even more arrays

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,10 +1,11 @@
 #include "main.h"
+#include <stdlib>
 /**
- * _strpbrk - finds a string for any of the set
- *@s: the string to search
+ * _strpbrk - finds a string for any of the set bytes
+ * @s: the string to search
  * @accept: the bytes to search for
- * Return: Null if set is not matched
- * or pointer to the matched byte
+ * Return: a pointer to the byte in 
+ * @accept, or 	NULL if no such byte is found.
  */
 char *_strpbrk(char *s, char *accept)
 {
