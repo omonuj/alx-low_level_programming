863f2d1985c6df4f6a8a18db28242f5410d386cb
omonuj <jonahodoh@gmail.com>
2023-04-03 19:51:25 -0700

    even more arrays

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diasums - prints the sum of the diagonals
+ * @a: the name of the array
+ * Return: nothig
+ */
+void print_diagsums(int *a, int size)
+{
+	int i = 0;
+	int j = size - 1;
+	int sum1 = 0;
+	int sum2 = 0;
+
+	while (i <= (size * size))
+	{
+		sum1 = sum1 + a[i];
+		i = i + size + 1;
+	}
+
+	while (j < (size * size - 1))
+	{
+		sum2 += a[j];
+		j = j + size - 1;
+	}
+
+	printf("%d\n", sum1, sum2);
+}
