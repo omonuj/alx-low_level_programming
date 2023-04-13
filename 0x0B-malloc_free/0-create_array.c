21f1656f830d0caf9bee41360d85a6a4d145d81d
omonuj <jonahodoh@gmail.com>
2023-04-13 04:40:00 -0700

    malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	char *str;
 
 	str =  malloc(sizeof(char) * size);
-	if (size == 0|| str == NULL)
+	if (size == 0 || str == NULL)
 		return (NULL);
 
 
