704ea3c2998f31510ddffb94337b366b0e1c5f08
omonuj <jonahodoh@gmail.com>
2023-04-21 19:01:12 -0700

    variadics

--- 0x0F-function_pointers/0x10-variadic_functions/0-sum_them_all.c
+++ 0x0F-function_pointers/0x10-variadic_functions/0-sum_them_all.c
@@ -8,18 +8,17 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list valist;
-	unsigned int sum = 0, i;
+	va_list ap;
+
+	int sum;
+	unsigned int i;
 
 	if (n == 0)
 		return (0);
-
-	va_start(valist, n);
-
-	for (i = 0; i < n; i++)
-		sum += va_arg(valist, int);
-
-	va_end(valist);
-
+	sum = 0;
+	va_start(ap, n);
+		for (i = 0; i < n; i++)
+			sum += va_arg(ap, int);
+	va_end(ap);
 	return (sum);
 }
